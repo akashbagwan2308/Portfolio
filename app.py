3e238c83b147075b24140e2363f16894de75ada6
Akash Bagwan <110125088+akashbagwan2308@users.noreply.github.com>
2024-01-01 23:18:33 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('project.html')
+
+# New route for handling 404 errors
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
